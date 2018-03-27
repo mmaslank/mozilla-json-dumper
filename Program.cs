25e9b3076c24bdd3bde1b88c69d7cd7fae24b580
mmaslank <michal.maslanka.7@gmail.com>
2018-03-27 17:19:36 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace UACTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var line in Directory.GetDirectories(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)))
+                if (line.Contains("Mozilla"))
+                    foreach (var directory in Directory.GetDirectories(line, "*", SearchOption.AllDirectories))
+                        foreach (var file in Directory.GetFiles(directory, "*.json"))
+                            using (System.IO.StreamWriter targetFile = new System.IO.StreamWriter(@"target.txt", true))
+                            {
+                                targetFile.WriteLine(File.ReadAllText(file));
+                            }
+            //Console.WriteLine(File.ReadAllText(file));
+        }
+    }
+}
